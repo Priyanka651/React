6654a825a418d66a52a2b5c481030c180aaf4f3d
Priyanka651 <Pariprajapati2109@gmail.com>
2025-02-18 23:22:12 -0800

    Create Previous_code.js

---
+++ Creating and importing components/Previous_code.js
@@ -0,0 +1,20 @@
+function Heading() {
+  return (
+    <h1></h1>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      This is the starting code for "Your first component" ungraded lab
+      <Heading />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
